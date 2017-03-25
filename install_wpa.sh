6f4f7562f6fd5e0fda71791f2390136e5a6632b5
Namila Bandara <namila007@gmail.com>
2017-03-25 22:09:55 +0530

    updated

--- install_wpa.sh
+++ install_wpa.sh
@@ -14,14 +14,14 @@ if [ "$(whoami)" != "root" ]; then
 fi 
 
 set_dl(){
-echo "deb http://london.mirrors.linode.com/ubuntu/ vivid main" | sudo tee /etc/apt/sources.list.d/vivid.list
-echo -e "Package: *\nPin: release o=Ubuntu,n=vivid\nPin-Priority: -1" | sudo tee cat /etc/apt/preferences.d/vivid
+	echo "deb http://london.mirrors.linode.com/ubuntu/ vivid main" | sudo tee /etc/apt/sources.list.d/vivid.list
+	echo -e "Package: *\nPin: release o=Ubuntu,n=vivid\nPin-Priority: -1" | sudo tee cat /etc/apt/preferences.d/vivid
 }
 
 update(){
-sudo apt-get update
-sudo apt-get install wpasupplicant=2.1-0ubuntu7
-sudo apt-mark hold 
+	sudo apt-get update
+	sudo apt-get install wpasupplicant=2.1-0ubuntu7
+	sudo apt-mark hold 
 }
 
 error_update(){
@@ -30,7 +30,7 @@ error_update(){
 }
 
 restart(){
-sudo /etc/init.d/network-manager restart
+	sudo /etc/init.d/network-manager restart
 }
 
 
